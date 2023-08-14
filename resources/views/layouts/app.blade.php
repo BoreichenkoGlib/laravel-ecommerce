682d0c93a1cb133d80272c59c61d234f712090c6
BoreichenkoGlib <boreichenkoglib@gmail.com>
2023-08-14 13:53:03 +0300

    app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <link rel="icon" href="public/favicon.ico" />
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('public/favicon.ico') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
